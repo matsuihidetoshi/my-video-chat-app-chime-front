bead19ba776cb3579cd2446c57059305821d1577
松井英俊 <dev.matsuihidetoshi@gmail.com>
2021-02-21 15:21:13 +0900

    add icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pwa:{
+    name:'Shrimphouse',
+    themeColor:'#eb539e',
+    appleMobileWebAppCapable:'yes',
+    appleMobileWebAppStatusBarStyle:'default'
+  }
 }
