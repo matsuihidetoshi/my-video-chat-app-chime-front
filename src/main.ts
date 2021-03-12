3af5ac6cae507d82a648871f4c13cc43dadb5691
松井英俊 <dev.matsuihidetoshi@gmail.com>
2021-03-12 20:20:26 +0900

    [Fix]remove comment

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,15 @@ import { AmplifyPlugin, components } from 'aws-amplify-vue'
 Vue.config.productionTip = false
 
 const awsmobile: any = {
-  "aws_project_region": process.env.VUE_APP_SHRIMPHOUSE_AWS_PROJECT_REGION,// "ap-northeast-1",
-  "aws_cognito_identity_pool_id": process.env.VUE_APP_SHRIMPHOUSE_AWS_COGNITO_IDENTITY_POOL_ID,// "ap-northeast-1:fd7b99b7-23db-412e-87ba-b755ecf20941",
-  "aws_cognito_region": process.env.VUE_APP_SHRIMPHOUSE_AWS_COGNITO_REGION,// "ap-northeast-1",
-  "aws_user_pools_id": process.env.VUE_APP_SHRIMPHOUSE_AWS_USER_POOLS_ID,//"ap-northeast-1_bThIAXc2j",
-  "aws_user_pools_web_client_id": process.env.VUE_APP_SHRIMPHOUSE_AWS_USER_POOLS_WEB_CLIENT_ID,//"3omdrkqr6aj7upt8gd5kdff4m8",
+  "aws_project_region": process.env.VUE_APP_SHRIMPHOUSE_AWS_PROJECT_REGION,
+  "aws_cognito_identity_pool_id": process.env.VUE_APP_SHRIMPHOUSE_AWS_COGNITO_IDENTITY_POOL_ID,
+  "aws_cognito_region": process.env.VUE_APP_SHRIMPHOUSE_AWS_COGNITO_REGION,
+  "aws_user_pools_id": process.env.VUE_APP_SHRIMPHOUSE_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.VUE_APP_SHRIMPHOUSE_AWS_USER_POOLS_WEB_CLIENT_ID,
   "oauth": {},
-  "aws_appsync_graphqlEndpoint": process.env.VUE_APP_SHRIMPHOUSE_AWS_APPSYNC_GRAPHQLENDPOINT,//"https://h2ncb73ukvfilhx6piojgd5cpm.appsync-api.ap-northeast-1.amazonaws.com/graphql",
-  "aws_appsync_region": process.env.VUE_APP_SHRIMPHOUSE_AWS_APPSYNC_REGION,//"ap-northeast-1",
-  "aws_appsync_authenticationType": process.env.VUE_APP_SHRIMPHOUSE_AWS_APPSYNC_AUTHENTICATIONTYPE//"AMAZON_COGNITO_USER_POOLS"
+  "aws_appsync_graphqlEndpoint": process.env.VUE_APP_SHRIMPHOUSE_AWS_APPSYNC_GRAPHQLENDPOINT,
+  "aws_appsync_region": process.env.VUE_APP_SHRIMPHOUSE_AWS_APPSYNC_REGION,
+  "aws_appsync_authenticationType": process.env.VUE_APP_SHRIMPHOUSE_AWS_APPSYNC_AUTHENTICATIONTYPE
 }
 
 Amplify.configure(awsmobile)
